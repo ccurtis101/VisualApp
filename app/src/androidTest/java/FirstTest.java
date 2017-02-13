8ac63aa9fbd0979555fa11af8241b6f41319e069
Chris Curtis <chriscurtis@MacBook-Pro.local>
2017-02-13 19:19:16 +0100

    added a unittestJavaClass and added dependency to build.gradle file

---
+++ app/src/androidTest/java/FirstTest.java
@@ -0,0 +1,20 @@
+/**
+ * Created by chriscurtis on 13/02/2017.
+ */
+////https://developer.android.com/training/testing/unit-testing/local-unit-tests.html#build
+
+////http://wptrafficanalyzer.in/blog/android-testing-example-helloworld-with-activityunittestcase/
+
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class FirstTest {
+
+
+
+
+
+}
